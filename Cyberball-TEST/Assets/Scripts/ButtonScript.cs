c45ddf282b914cd507980b399ce42b1ddb169571
Jisha-George <jishathakidiyel@yahoo.com>
2019-08-21 23:40:36 +0100

    new?

---
+++ Cyberball-TEST/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour {
+    
+    public void PlayB()
+    {
+        //load play
+        SceneManager.LoadScene("Play");
+    }    
+
+    public void SettingsB()
+    {
+        //load settings
+        SceneManager.LoadScene("Settings");
+    }
+
+
+
+    public void BackB()
+    {
+
+    }
+
+    public void StartB()
+    {
+        SceneManager.LoadScene("Child");
+    }
+
+    public void QuitB()
+    {
+        //quit
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
