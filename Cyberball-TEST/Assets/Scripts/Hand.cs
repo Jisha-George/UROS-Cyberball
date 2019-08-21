c53e449c39f460d4bf1874b7d0abf3c9e84164da
Jisha-George <jishathakidiyel@yahoo.com>
2019-08-21 14:07:06 +0100

    UI Buttons

---
+++ Cyberball-TEST/Assets/Scripts/Hand.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+
+    public GameObject parent;
+    public GameObject PlayerBall;
+    public GameObject movingBall;
+    static Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        PlayerBall.SetActive(false);
+        anim.SetBool("Ball", false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 BPos = PlayerBall.transform.position;
+        Vector3 RPos = movingBall.transform.position;
+        
+        if (Vector3.Distance(RPos, BPos) <= 200 && Vector3.Distance(RPos,BPos) >= 190)
+        {
+            anim.SetTrigger("Catching");
+        }
+    }
+
+    public void BallCatch() {
+        PlayerBall.SetActive(true);
+        PlayerBall.transform.parent = parent.transform;
+        movingBall.SetActive(false);
+        anim.SetBool("Ball", true);
+    }
+
+    private void OnMouseDown()
+    {
+       if (anim.GetBool("Ball")){
+            anim.SetTrigger("Throwing");
+       }
+    }
+}
--- TEST/Assets/Scripts/ButtonScript.cs
+++ TEST/Assets/Scripts/ButtonScript.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void PlayB()
+    {
+        //load play
+        SceneManager.LoadScene("Play");
+    }    
+
+    public void SettingsB()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
 }
