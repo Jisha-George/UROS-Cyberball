2cbe98864a303c03e7702d5b440e9176a272a7c3
Jisha-George <36167735+Jisha-George@users.noreply.github.com>
2019-10-11 23:48:14 +0100

    JSON reading

--- JSON_TEST/Assets/Scripts/Data.cs
+++ JSON_TEST/Assets/Scripts/Data.cs
@@ -1,13 +1,34 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 
 //[System.Serializable]
-//public class Data {
+public class Data : MonoBehaviour
+{
 
-//    GameData gameData = new GameData();
-//    public string Age, Gen, GM, R, P;
+    //    GameData gameData = new GameData();
+    //    public string Age, Gen, GM, R, P;
 
-//}
+    public Text stuff;
+    public Text blank;
+    public JSONData t;
+
+    void Start()
+    {
+        t = new JSONData("C:/Users/computing/AppData/LocalLow/UROS/JSON_TEST/data.json");
+        stuff.text = t.Age + " " + t.GameMode + " " + t.Gender + " " + t.Rounds;
+
+
+        if(t.GameMode == "Inclusive")
+        {
+            blank.text = "hi";
+        }
+        else
+        {
+            blank.text = "bye";
+        }
+    }   
+
+}
